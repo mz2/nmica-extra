8886daf547e2b8e27281b6792d1c4868cafb2ac2
Matias Piipari <matias.piipari@gmail.com>
2009-04-30 10:16:55 +0000

    Added a tool for outputting motif sets in other output formats apart from TRANSFAC

---
+++ src/net/derkholm/nmica/extra/app/MotifSetConverter.java
@@ -0,0 +1,58 @@
+package net.derkholm.nmica.extra.app;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.derkholm.nmica.build.NMExtraApp;
+import net.derkholm.nmica.build.VirtualMachine;
+import net.derkholm.nmica.motif.Motif;
+import net.derkholm.nmica.motif.MotifIOTools;
+
+import org.bjv2.util.cli.App;
+import org.bjv2.util.cli.Option;
+import org.bjv2.util.cli.UserLevel;
+
+@App(overview = "Output XMS files in other formats", generateStub = true)
+@NMExtraApp(launchName = "nmconvertxms", vm = VirtualMachine.SERVER)
+public class MotifSetConverter {
+	private OutputStream outputStream = System.out;
+	private OutputFormat outputFormat;
+	
+	private enum OutputFormat {
+		TRANSFAC
+	}
+
+	@Option(help = "Output filename")
+	public void setOut(String str) throws FileNotFoundException {
+		this.outputStream = new BufferedOutputStream(new FileOutputStream(new File(str)));
+	}
+	
+	@Option(help = "Output format", userLevel = UserLevel.EXPERT)
+	public void setFormat(OutputFormat format) {
+		this.outputFormat = format;
+	}
+
+	public void main(String[] args) throws Exception {
+		List<Motif> motifList = new ArrayList<Motif>();
+		for (String filen : args) {
+			FileReader fileReader = new FileReader(new File(filen));
+			Motif[] motifs = MotifIOTools.loadMotifSetXML(fileReader);
+			if (fileReader != null)
+				fileReader.close();
+			for (Motif m : motifs)
+				motifList.add(m);
+		}
+
+		if (outputFormat == OutputFormat.TRANSFAC) {
+			MotifIOTools.writeMotifSetTRANSFACFormat(
+					outputStream, 
+					motifList.toArray(new Motif[motifList.size()]));			
+		}
+	}
+}
