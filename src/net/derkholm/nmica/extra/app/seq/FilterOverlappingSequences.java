7ebb27baa5ebd85f9006b8671001643571a4a85e
Matias Piipari <matias.piipari@gmail.com>
2009-07-30 11:45:25 +0000

    Replaced the nmoverlapseq implementation with a simpler one

--- src/net/derkholm/nmica/extra/app/seq/FilterOverlappingSequences.java
+++ src/net/derkholm/nmica/extra/app/seq/FilterOverlappingSequences.java
@@ -3,15 +3,12 @@
  */
 package net.derkholm.nmica.extra.app.seq;
 
+import gfftools.GFFUtils;
+
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import net.derkholm.nmica.build.NMExtraApp;
@@ -21,127 +18,81 @@ import org.biojava.bio.program.gff.GFFDocumentHandler;
 import org.biojava.bio.program.gff.GFFParser;
 import org.biojava.bio.program.gff.GFFRecord;
 import org.biojava.bio.program.gff.GFFWriter;
-import org.biojava.bio.program.gff.SimpleGFFRecord;
+import org.biojava.bio.symbol.Location;
+import org.biojava.bio.symbol.LocationTools;
+import org.biojava.bio.symbol.RangeLocation;
 import org.bjv2.util.cli.App;
 import org.bjv2.util.cli.Option;
 
-@App(overview = 
-	"Write out sequence regions covered by GFF features " +
-	"in all input files with the specified count of overlapping nucleotides", 
-	generateStub = true)
+import biobits.utils.IOTools;
+
+@App(overview = "Write out sequence regions covered by GFF features", generateStub = true)
 @NMExtraApp(launchName = "nmoverlapseq", vm = VirtualMachine.SERVER)
 public class FilterOverlappingSequences {
-    private String masterBrandKey = null;
-    private String outputBrandKey = null;
-    private int minOverlap = 1;
-        
-    @Option(help="Minimum overlap between features (default=1)")
-    public void setMinOverlap(int i) {
-        this.minOverlap = i;
-    }
-    
-    public void setMasterBrandKey(String masterBrandKey) {
-        this.masterBrandKey = masterBrandKey;
-    }
-
-    public void setOutputBrandKey(String outputBrandKey) {
-        this.outputBrandKey = outputBrandKey;
-    }
-
-    private static final class BrandedSpan implements Comparable<BrandedSpan> {
-        public final int min;
-        public final int max;
-        public final String brand;
-        
-        private BrandedSpan(int min, int max, String brand) {
-            this.min = min;
-            this.max = max;
-            this.brand = brand;
+	private boolean ignoreNames = false;
+	private boolean fraction = false;
+	private File featuresFile;
+	private File maskFile;
+	
+	@Option(help="The features that are filtered and output " +
+			"(those that overlap the masking features are output)")
+	public void setFeatures(File f) {
+		this.featuresFile = f;
+	}
+	
+	@Option(help="The features that are used as a mask")
+	public void setMask(File f) {
+		this.maskFile = f;
+	}
+	
+	public void setFraction(boolean b) {
+		this.fraction = b;
+	}
+	
+	public void setIgnoreNames(boolean b) {
+		this.ignoreNames = b;
+	}
+	
+    public void main(String[] args) throws Exception {
+        final Map<String,Location> locsByChr = GFFUtils.gffToLocationMap(maskFile);
+        final Location allloc;
+        if (ignoreNames) {
+        	allloc = LocationTools.union(locsByChr.values());
+        } else {
+        	allloc = null;
         }
-
-        public int compareTo(BrandedSpan arg0) {
-            int dif = min - arg0.min;
-            if (dif != 0) {
-                return dif;
-            } 
-            dif = max - arg0.max;
-            if (dif != 0) {
-                return dif;
-            }
-            return brand.compareTo(arg0.brand);
-        }        
-    }
-    
-    public void main(String[] args)
-        throws Exception
-    {
-        File ref = new File(args[0]);
-        File f = new File(args[1]);
         
-        final Map<String,List<BrandedSpan>> locsByChr = new HashMap<String,List<BrandedSpan>>();
         GFFParser gffp = new GFFParser();
-        BufferedReader refGff = new BufferedReader(new FileReader(ref));
-        gffp.parse(refGff, new GFFDocumentHandler() {
-            public void startDocument(String locator) {
-            }
-
-            public void endDocument() {
-            }
-
-            public void commentLine(String comment) {
-            }
-
-            public void recordLine(GFFRecord record) {
-                String brand;
-                if (masterBrandKey != null) {
-                	try {
-                		brand = ((List) record.getGroupAttributes().get(masterBrandKey)).get(0).toString();
-                	} catch (Exception ex) {return;}
-                } else {
-                    brand = record.getFeature() + "_" + record.getSeqName() + "_" + record.getStart();
-                }
-                BrandedSpan b = new BrandedSpan(record.getStart(), record.getEnd(), brand);
-                List<BrandedSpan> l = locsByChr.get(record.getSeqName());
-                if (l == null) {
-                    l = new ArrayList<BrandedSpan>();
-                    locsByChr.put(record.getSeqName(), l);
-                }
-                l.add(b);
-            }
-        });
-        
-        for (List<BrandedSpan> l : locsByChr.values()) {
-            Collections.sort(l);
-        }
-        
-        
-        BufferedReader gff = new BufferedReader(new FileReader(f));
+        BufferedReader gff = IOTools.fileBufferedReader(featuresFile);
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
         final GFFWriter gffw = new GFFWriter(pw);
         gffp.parse(gff, new GFFDocumentHandler() {
+        	int all = 0, accepted= 0;
+        	
             public void recordLine(GFFRecord record) {
-                List<BrandedSpan> refLocs = locsByChr.get(record.getSeqName());
-                if (refLocs == null) {
-                    return;
+                Location refLoc = allloc == null ? locsByChr.get(record.getSeqName()) : allloc;
+                if (refLoc == null) {
+                	String n = record.getSeqName();
+                	if (n.startsWith("chr")) {
+                		n = n.substring(3);
+                	}
+                	refLoc = locsByChr.get(n);
+                	if (refLoc == null) {
+                		return;
+                	}
                 }
-                
-                int lb = 0, ub = refLocs.size() - 1;
-                while (ub >= lb) {
-                    int mid = (lb + ub) / 2;
-                    BrandedSpan midSpan = refLocs.get(mid);
-                    if (midSpan.min > record.getEnd()) {
-                        ub = mid - 1;
-                    } else if (midSpan.max < record.getStart()) {
-                        lb = mid + 1;
-                    } else {
-                        int overlap = Math.min(midSpan.max, record.getEnd()) - Math.max(midSpan.min, record.getStart()) + 1;
-                        if (overlap >= minOverlap) {
-                            SimpleGFFRecord r = new SimpleGFFRecord(record);
-                            r.getGroupAttributes().put(outputBrandKey, Collections.singletonList(midSpan.brand));
-                            gffw.recordLine(r);
-                        }
-                        return;
-                    }
+                int min = record.getStart();
+                int max = record.getEnd();
+                if (max < min) {
+                	min = record.getEnd();
+                	max = record.getStart();
+                }
+                Location loc = new RangeLocation(min, max);
+                ++all;
+                if (LocationTools.overlaps(loc, refLoc)) {
+                	if (!fraction)
+                		gffw.recordLine(record);
+                    ++accepted;
                 }
             }
             
@@ -151,6 +102,10 @@ public class FilterOverlappingSequences {
             
             public void endDocument() {
                 gffw.endDocument();
+                
+                if (fraction) {
+                	System.out.println((1.0 * accepted) / all);
+                }
             }
             
             public void commentLine(String comment) {
@@ -158,6 +113,5 @@ public class FilterOverlappingSequences {
             }
         }, "");
         pw.flush();
-    
     }
 }
