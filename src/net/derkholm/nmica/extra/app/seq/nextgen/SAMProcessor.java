66662a2763e1af40d665b4cd0a7c8a8ff4558fe0
Matias Piipari <matias.piipari@gmail.com>
2009-08-25 16:18:10 +0000

    Added more debugging statements

--- src/net/derkholm/nmica/extra/app/seq/nextgen/SAMProcessor.java
+++ src/net/derkholm/nmica/extra/app/seq/nextgen/SAMProcessor.java
@@ -250,6 +250,7 @@ public abstract class SAMProcessor {
 		int winEnd = windowCenter + (windowSize / 2);
 		
 		while (recIterator.hasNext()) {
+			System.err.println("-");
 			SAMRecord rec = recIterator.next();
 			if (rec.getMappingQuality() < this.qualityCutoff) continue;
 			
