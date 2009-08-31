d711cd9632eebbb7ce76bb74fb41517ff7f2cb6d
Matias Piipari <matias.piipari@gmail.com>
2009-08-31 23:59:14 +0000

    Added a single prepared statement.

--- src/net/derkholm/nmica/extra/app/seq/nextgen/CombineDepths.java
+++ src/net/derkholm/nmica/extra/app/seq/nextgen/CombineDepths.java
@@ -3,26 +3,29 @@ package net.derkholm.nmica.extra.app.seq.nextgen;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import net.derkholm.nmica.build.NMExtraApp;
 import net.derkholm.nmica.build.VirtualMachine;
+import net.derkholm.nmica.extra.app.seq.nextgen.CountDepths.Format;
 
 import org.bjv2.util.cli.App;
 import org.bjv2.util.cli.Option;
 
 @NMExtraApp(launchName = "ngcombinedepths", vm = VirtualMachine.SERVER)
-@App(overview = "Combine depths to a single SQLite database", generateStub = true)
+@App(overview = "Combine depths from individual SQLite databases to a single one", generateStub = true)
 public class CombineDepths {
 	private File outputFile;
 	private Connection connection;
 
-	@Option(help="Output file")
+	@Option(help="Output file (required if output format is SQLite)", optional=true)
 	public void setOut(File f) {
 		this.outputFile = f;
 	}
+	
 	
 	private Connection connection() throws SQLException, ClassNotFoundException {
 		Class.forName("org.sqlite.JDBC");
@@ -40,6 +43,7 @@ public class CombineDepths {
 	
 	public void main(String[] args) throws SQLException, ClassNotFoundException {
 		CountDepths.createDepthDatabase(connection());
+		PreparedStatement stat = CountDepths.insertDepthEntryStatement(this.connection());
 		
 		for (String inFileName : args) {
 			System.err.printf("Adding %s...%n", inFileName);
@@ -50,7 +54,8 @@ public class CombineDepths {
 			Statement statement = conn.createStatement();
 			ResultSet results = statement.executeQuery(
 				"SELECT ref_name,begin_coord,end_coord,depth,pvalue FROM window;");
-			
+
+			int i = 1;
 			while (results.next()) {
 				String refName = results.getString(1);
 				int begin = results.getInt(2);
@@ -58,7 +63,23 @@ public class CombineDepths {
 				double d = results.getDouble(4);
 				double p = results.getDouble(5);
 				
-				System.err.printf("%s %d %d %.2f %.2f%n",refName,begin,end,d,p);
+				/*
+				System.out.printf(
+						"%s\t%d\t%d\t%d\t%d\t%.8f%n", 
+						i++, 
+						refName, 
+						begin, 
+						end, 
+						d, 
+						p);
+						*/
+				stat.setInt(1, i++);
+				stat.setString(2, refName);
+				stat.setInt(3, begin);
+				stat.setInt(4, end);
+				stat.setDouble(5, d);
+				stat.setDouble(6, p);
+				stat.executeUpdate();
 			}
 			results.close();
 		}
