cd3ed0f5500f738c1c0251d1ca96ae1ab834f960
Matias Piipari <matias.piipari@gmail.com>
2009-09-01 16:46:07 +0000

    - Added hsqldb.jar

--- src/net/derkholm/nmica/extra/app/seq/nextgen/CountDepths.java
+++ src/net/derkholm/nmica/extra/app/seq/nextgen/CountDepths.java
@@ -31,24 +31,23 @@ import cern.jet.random.engine.RandomEngine;
 @App(overview = "Output sequencing depth inside a window.", generateStub = true)
 public class CountDepths extends SAMProcessor {
 	public enum Format {
-		SQLITE,
-		TSV
+		HSQLDB, TSV
 	}
-	
+
 	private Format format = Format.TSV;
 	private int windowIndex;
 	private Map<String, Poisson> nullDistributions = new HashMap<String, Poisson>();
-	
+
 	private File outputFile;
 	private Connection connection;
 	private PreparedStatement insertDepthEntryStatement;
-	
+
 	private RandomEngine randomEngine = RandomEngine.makeDefault();
 	private HashMap<String, Integer> refIds;
 	private List<String> refSeqNames;
-	
+
 	@Override
-	@Option(help="Reference sequence lengths")
+	@Option(help = "Reference sequence lengths")
 	public void setRefLengths(File f) throws BioException, IOException {
 		try {
 			super.setRefLengths(f);
@@ -57,104 +56,109 @@ public class CountDepths extends SAMProcessor {
 		}
 		this.refSeqNames = SAMProcessor.parseRefNamesFromRefLengthFile(f);
 	}
-	
+
 	@Override
-	@Option(help="Read counts per reference sequence")
+	@Option(help = "Read counts per reference sequence")
 	public void setReadCounts(File f) {
 		super.setReadCounts(f);
 	}
-	
-	@Option(help="Output format")
+
+	@Option(help = "Output format")
 	public void setFormat(Format format) {
 		this.format = format;
 	}
-	
-	@Option(help="Output file " +
-			"(suffixed with _x where x is LSB job index " +
-			"if run on LSF as part of a job array)")
+
+	@Option(help = "Output file "
+			+ "(suffixed with _x where x is LSB job index "
+			+ "if run on LSF as part of a job array)")
 	public void setOut(File f) {
 		if (jobIndex() >= 0) {
-			this.outputFile = new File(String.format("%s_%d",f.getPath(),this.jobIndex()));
+			this.outputFile = new File(String.format("%s_%d", f.getPath(), this
+					.jobIndex()));
 		} else {
 			this.outputFile = f;
 		}
 	}
-	
+
 	@Override
-	@Option(help="Mapped reads")
+	@Option(help = "Mapped reads")
 	public void setMap(String in) {
 		super.setMap(in);
 	}
-	
+
 	@Override
-	@Option(help="Index file for mapped reads")
+	@Option(help = "Index file for mapped reads")
 	public void setIndex(File f) {
 		super.setIndex(f);
 	}
-	
+
 	@Override
-	@Option(help="Extended length")
+	@Option(help = "Extended length")
 	public void setExtendTo(int i) {
 		super.setExtendTo(i);
 	}
-	
+
 	private Connection connection() throws SQLException {
 		if (this.connection == null) {
-			this.connection = 
-				DriverManager.getConnection(
-					String.format(
-						"jdbc:sqlite:%s",
-						this.outputFile.getPath()));
+			this.connection = DriverManager.getConnection(String.format(
+					"jdbc:hsqldb:file:%s", this.outputFile.getPath()), "sa", "");
 
 			this.connection.setAutoCommit(true);
 		}
 		return this.connection;
 	}
-	
+
 	private PreparedStatement insertDepthEntryStatement() throws SQLException {
 		if (this.insertDepthEntryStatement == null) {
-			this.insertDepthEntryStatement = CountDepths.insertDepthEntryStatement(this.connection());
+			this.insertDepthEntryStatement = CountDepths
+					.insertDepthEntryStatement(this.connection());
 		}
 		return this.insertDepthEntryStatement;
 	}
-	
-	public static PreparedStatement insertDepthEntryStatement(Connection conn) throws SQLException {
-		return conn.prepareStatement(
-        	"INSERT INTO window VALUES (?, ?, ?, ?, ?, ?);");
+
+	public static PreparedStatement insertDepthEntryStatement(Connection conn)
+			throws SQLException {
+		return conn
+				.prepareStatement("INSERT INTO window VALUES (?, ?, ?, ?, ?, ?);");
 	}
-		
+
 	private void initNullDistributions() {
 		for (String name : this.refSeqLengths.keySet()) {
-			System.err.printf("%s count: %d read_extended_length:%d ref_length:%d window_size:%d ", 
-					name,
-					readCounts.get(name),
-					this.extendedLength,
-					this.refSeqLengths.get(name),
-					this.windowSize);
-			double lambda = 
-				(double)this.readCounts.get(name) * this.extendedLength /
-				(double)this.refSeqLengths.get(name)
-				 * 
-				(double)this.windowSize;
-			
+			System.err
+					.printf(
+							"%s count: %d read_extended_length:%d ref_length:%d window_size:%d ",
+							name, readCounts.get(name), this.extendedLength,
+							this.refSeqLengths.get(name), this.windowSize);
+			double lambda = (double) this.readCounts.get(name)
+					* this.extendedLength
+					/ (double) this.refSeqLengths.get(name)
+					* (double) this.windowSize;
+
 			System.err.println("lambda:" + lambda);
-			nullDistributions.put(
-					name, 
-					new Poisson(lambda, randomEngine));
-		}	
+			nullDistributions.put(name, new Poisson(lambda, randomEngine));
+		}
 	}
 	
-	
+	 public void shutdown() throws SQLException {
+
+	        Statement st = connection().createStatement();
+
+	        st.execute("SHUTDOWN");
+	        connection().close();    // if there are no other open connection
+	    }
+
+
+
 	@Override
-	public void main(String[] args) throws BioException, ClassNotFoundException, SQLException {
+	public void main(String[] args) throws BioException,
+			ClassNotFoundException, SQLException {
 		initNullDistributions();
-		
+
 		System.err.println("");
-		if (format == Format.SQLITE) {
-			Class.forName("org.sqlite.JDBC");
+		if (format == Format.HSQLDB) {
 			CountDepths.createDepthTable(this.connection());
 		}
-		
+
 		this.windowIndex = 0;
 		System.err.println("Opening indexed reads...");
 		SAMFileReader reader = new SAMFileReader(new File(in), indexFile);
@@ -163,100 +167,96 @@ public class CountDepths extends SAMProcessor {
 		System.err.println("SAM file and index read");
 		int id = 0;
 		for (String name : this.refSeqLengths.keySet()) {
-			System.err.printf("Calculating pileup for %s%n",name);
+			System.err.printf("Calculating pileup for %s%n", name);
 			int refId = getRefId(name);
 			Poisson nullDist = this.nullDistributions.get(name);
-			
-			SAMPileup pileup = 
-				new SAMPileup(name, this.refSeqLengths.get(name), this.extendedLength);
 
-			CloseableIterator<SAMRecord> recIterator = 
-				reader.queryOverlapping(name, 0, this.refSeqLengths.get(name));
-			while (recIterator.hasNext()) {pileup.add(recIterator.next());}
+			SAMPileup pileup = new SAMPileup(name,
+					this.refSeqLengths.get(name), this.extendedLength);
+
+			CloseableIterator<SAMRecord> recIterator = reader.queryOverlapping(
+					name, 0, this.refSeqLengths.get(name));
+			while (recIterator.hasNext()) {
+				pileup.add(recIterator.next());
+			}
 			recIterator.close();
-			
+
 			System.err.println("Storing pileup data to database...");
 			PreparedStatement ins = insertDepthEntryStatement();
 			System.err.println("Iterating through");
-			for (int i = 0,len=this.refSeqLengths.get(name); i < len; i++) {
+			for (int i = 0, len = this.refSeqLengths.get(name); i < len; i++) {
 				int depth = pileup.depthAt(i);
-				
+
 				if (depth > 0) {
 					System.err.println(depth);
 					ins.setInt(1, id++);
 					ins.setInt(2, refId);
 					ins.setInt(3, i);
-					ins.setInt(4, i+extendedLength);
-					ins.setDouble(5, (double)depth);
+					ins.setInt(4, i + extendedLength);
+					ins.setDouble(5, (double) depth);
 					ins.setDouble(6, 1.0 - nullDist.cdf(depth));
 					ins.addBatch();
-					
+
 					if ((id % 100) == 0) {
 						System.err.println("Committing");
-						ins.executeBatch();						
+						ins.executeBatch();
 					}
 				}
 			}
 			System.err.println("Done.");
 		}
-		//connection().setAutoCommit(false);
-		
-		process();
-		//insertDepthEntryStatement().executeBatch();
-		//insertDepthEntryStatement().close();
-		//connection().setAutoCommit(false);
-		
-		this.connection().close();
+		// connection().setAutoCommit(false);
+
+		//process();
+		// insertDepthEntryStatement().executeBatch();
+		// insertDepthEntryStatement().close();
+		// connection().setAutoCommit(false);
+
+		this.shutdown();
 	}
-	
+
 	public static void createDepthTable(Connection conn) throws SQLException {
 		Statement stat = conn.createStatement();
 		stat.executeUpdate("DROP TABLE if exists window;");
-		stat.executeUpdate(
-			"CREATE TABLE window (" +
-				"id integer primary key autoincrement," +
-				"ref_id integer," +
-				"begin_coord integer," +
-				"end_coord integer," +
-				"depth float," +
-				"pvalue float);");
-		stat.executeUpdate("CREATE INDEX ref_name_begin_end_idx ON window(ref_id,begin_coord,end_coord);");
-		stat.executeUpdate("CREATE INDEX ref_name_begin_idx ON window(ref_id,begin_coord);");
-		stat.executeUpdate("CREATE INDEX ref_name_end_idx ON window(ref_id,end_coord);");
+		stat.executeUpdate("CREATE TABLE window ("
+				+ "id integer primary key autoincrement," + "ref_id integer,"
+				+ "begin_coord integer," + "end_coord integer,"
+				+ "depth float," + "pvalue float);");
+		stat
+				.executeUpdate("CREATE INDEX ref_name_begin_end_idx ON window(ref_id,begin_coord,end_coord);");
+		stat
+				.executeUpdate("CREATE INDEX ref_name_begin_idx ON window(ref_id,begin_coord);");
+		stat
+				.executeUpdate("CREATE INDEX ref_name_end_idx ON window(ref_id,end_coord);");
 		stat.close();
 	}
-	
 
 	@Override
-	public void process(final List<SAMRecord> recs, String refName, int begin, int end, int seqLength) {
+	public void process(final List<SAMRecord> recs, String refName, int begin,
+			int end, int seqLength) {
 		double avg = 0.0;
 		int depth = recs.size();
 		double pvalue = 1.0 - this.nullDistributions.get(refName).cdf(depth);
-		
+
 		if (depth > 0) {
 			if (format == Format.TSV) {
-				System.out.printf(
-					"%s\t%d\t%d\t%d\t%d\t%.8f%n",
-					refName,
-					this.windowIndex,
-					begin,
-					end,
-					depth,
-					pvalue);
+				System.out.printf("%s\t%d\t%d\t%d\t%d\t%.8f%n", refName,
+						this.windowIndex, begin, end, depth, pvalue);
 			} else {
 				PreparedStatement stat;
-				//System.err.printf("%s\t%d\t%d\t%d\t%d\t%.8f%n", refName, this.windowIndex, begin, end, depth, pvalue);				
-				
+				// System.err.printf("%s\t%d\t%d\t%d\t%d\t%.8f%n", refName,
+				// this.windowIndex, begin, end, depth, pvalue);
+
 			}
 		}
-		
+
 		this.windowIndex++;
 	}
-	
+
 	public int getRefId(String seqName) {
 		if (this.refIds == null) {
-			this.refIds = new HashMap<String,Integer>();
-			
+			this.refIds = new HashMap<String, Integer>();
+
 			int i = 0;
 			for (String name : this.refSeqNames) {
 				this.refIds.put(name, i++);
